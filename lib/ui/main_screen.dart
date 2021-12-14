5f3fbde1a97937597229ec71322a54c7d9a3dd64
fauzanafism <fauzan137@gmail.com>
2021-12-14 07:25:27 +0700

    first commit

---
+++ lib/ui/main_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff121214),
+      ),
+      body: Center(),
+    );
+  }
+}
