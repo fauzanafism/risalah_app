7e2dba7769ad7a1cfd3d6330c4491aaf04d220cb
fauzanafism <fauzan137@gmail.com>
2021-12-15 07:37:26 +0700

    completed basic UI

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -5,10 +5,118 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xff121214),
-        title: Image(image: AssetImage()),
+        flexibleSpace: Container(
+          // child: Text("Home", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  end: FractionalOffset.topLeft,
+                  begin: FractionalOffset.bottomRight,
+                  colors: [Colors.blue, Colors.purple])),
+        ),
+        title: Text("RISALAH"),
+        actions: [
+          Icon(Icons.person),
+          SizedBox(width: 20),
+          Icon(Icons.settings),
+          SizedBox(width: 15)
+        ],
       ),
-      body: Center(),
+      body: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            child: Column(
+              children: [
+                // Countdown waktu sholat
+                Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.red),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Text(
+                          '5 menit menjelang waktu maghrib',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                // Waktu sholat
+                Container(
+                  height: 80,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.green),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Text(
+                          'Waktu sholat',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Hadits of the day
+                    Container(
+                      height: 120,
+                      width: (MediaQuery.of(context).size.width / 2) - 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.brown[300]),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Center(
+                            child: Text(
+                              'Hadits',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      height: 120,
+                      width: (MediaQuery.of(context).size.width / 2) - 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.grey),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Center(
+                            child: Text(
+                              'Kultum',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
